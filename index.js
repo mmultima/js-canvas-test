912c76e8a8bc81433980f55cf0b5a2aced194e0d
Mikko Multimäki <mikko.multimaki@kolumbus.fi>
2017-05-24 08:46:22 +0300

    Not working keypress.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var myX = 10, myY = 10;
+
 var init = function() {
 	var myCanvas = document.getElementById("myCanvas");
 	if (myCanvas) {
@@ -5,8 +7,26 @@ var init = function() {
 			var coordDiv = document.getElementById("coords");
 			coordDiv.innerHTML = "X: " + event.clientX + " Y: " + event.clientY;
 			//console.log("X: " + event.clientX + " Y: " + event.clientY);
-		}
-		
+		};
+		myCanvas.onkeypress = function(event) {
+			if (event.charCode === 100) {
+				myX++;
+			}
+			if (event.charCode === 97) {
+				myX--;
+			}
+			if (event.charCode === 115) {
+				myY--;
+			}
+			if (event.charCode === 119) {
+				myY++;
+			}
+			
+			var ctx=myCanvas.getContext("2d");
+			ctx.beginPath();
+			ctx.arc(myX,myY,20,0,2*Math.PI);
+			ctx.stroke();
+		};
 	}
 	else {
 		setTimeout(init, 50);					
